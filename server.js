053cbf7cc9fde1669797a640a69e946e5db1bd90
Kevin Cross <141079791+octatesystems@users.noreply.github.com>
2024-01-13 16:46:43 +0100

    commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongodb.initDb((err, mongodb) => {
         console.log(err);
     } else {
         app.listen(port);
-        console.log(`Connected to DB and listening on port ${port}`);
+        console.log(`Connected to Database and listening on port ${port}`);
     }
 });
 
