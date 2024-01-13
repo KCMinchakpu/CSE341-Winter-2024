6b97fadcd352de8c3475e8fe9f46c335348631d2
Kevin Cross <141079791+octatesystems@users.noreply.github.com>
2024-01-13 19:26:20 +0100

    commit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 //Read (GET) all contacts in the database
 const getAllData = async (req, res) => {
-  
+    try {
         const result = await mongodb
             .getDatabase()
             .collection('contacts')
@@ -12,21 +12,27 @@ const getAllData = async (req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(contacts);
         });
-    };
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
 //Read (GET) one contact (based on Id) in the database
 const getSingleData = async (req, res) => {
-
+    try {
         const UserId = new ObjectId(req.params.id);
         const result = await mongodb
             .getDatabase()
             .collection('contacts')
             .find({ _id: UserId});
-            result.toArray().then((contacts) => {
+        result.toArray().then((contacts) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(contacts[0]);
         });
-    };
-
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
 
 
 
